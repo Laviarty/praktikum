d813f1271e02cc1227da38c272b4002e34985891
Mika <meiki.leja@gmail.com>
2018-05-23 13:03:55 +0200

    down

--- Source/download.php
+++ Source/download.php
@@ -3,8 +3,8 @@ function debug_to_consoled( $data ) {
     $output = $data;
     if ( is_array( $output ) )
         $output = implode( ',', $output);
-
     echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
+}
 	
 debug_to_consoled("gestartet");
 
@@ -14,23 +14,22 @@ if(!empty($files))
    {
 	$zipname = 'Expressionsanalyse.zip';
 	$zip = new ZipArchive;
-	$zip->open($zipname, ZipArchive::CREATE);
+	$zip->open($zipname, ZipArchive::CREATE | ZipArchive::OVERWRITE);
 	debug_to_consoled("zip created");
 	foreach ($files as $file) {
   		$zip->addFile($file);
 		}
 	$zip->close();
-
+	   
 	header('Content-type: application/zip');
 	header('Content-Disposition: attachment; filename='.$zipname);
-	header('Content-Length: ' . filesize($zipname));
-	header("Pragma: no-cache"); 
-	header("Expires: 0"); 
+        header('Expires: 0');
+        header('Pragma: no-cache');
+	header('Content-length: ' . filesize($zipname));
+	   ob_clean();
+	flush();
 	readfile($zipname);
 	exit;
 	}
    
-   
-   
-}
 ?>
